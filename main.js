c2e8d91cd7198e8cbb67f2cd6605a49a56e005ab
Hernan Y.Ke <yuriqiao@gmail.com>
2016-07-27 22:40:19 -0400

    refactor to function

--- main.js
+++ main.js
@@ -2,12 +2,21 @@
  * Created by Hernan Y.Ke on 2016/7/27.
  */
 var seneca = require('seneca')();
-seneca.add({role:"account",cmd:"login"},function(args,respond){
+function account(options){
+    this.add({init:"account"},function(pluginInfo,respond){
+        console.log(options.message);
+        respond();
+    })
 
-})
-seneca.add({role:"account",cmd:"register"},function(args,respond){
+    seneca.add({role:"account",cmd:"login"},function(args,respond){
 
-})
+    })
+    seneca.add({role:"account",cmd:"register"},function(args,respond){
+
+    })
+}
+seneca.use(account,{message:"added"})
+
 seneca.act({role:"account",cmd:"login",username:"ke",password:"ke"},function(args,respond){
 
 })
